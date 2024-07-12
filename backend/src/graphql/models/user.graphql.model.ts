6c4c567c5e5db3126ef2a1420d6758891b0a37fb
i.kłusek <J6ZkqwVUn>
2024-07-12 07:02:15 +0200

    NEG-06-Mutation for create User

--- backend/src/graphql/models/user.graphql.model.ts
+++ backend/src/graphql/models/user.graphql.model.ts
@@ -1,10 +1,30 @@
+type CreateUser = {
+  user: {
+    name: string;
+    age: number;
+  };
+};
+
 const typeDef = /* GraphQL */ `
+  # Query - Read operation
   type Query {
     user: User
   }
+
+  #Mutations  - CUD operations
+  type Mutation {
+    createUser(user: NewUserInput!): User
+  }
+
+  input NewUserInput {
+    name: String!
+    age: Int
+  }
+
   type User {
     id: Int
     name: String
+    age: Int
   }
 `;
 
@@ -17,9 +37,14 @@ const resolvers = {
       };
     },
   },
+  Mutation: {
+    createUser: (_: null, { user }: CreateUser) => {
+      return { id: 1, ...user };
+    },
+  },
   //When type user, return field name with UpperCase
   User: {
-    name: (obj: { name: string }) => obj.name.toUpperCase(),
+    name: (obj: { name: string }) => obj.name.trim().toUpperCase(),
   },
 };
 
